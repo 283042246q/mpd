8f848d1c0ff4e9f7b93c58bcfbb96539ec125c80
Eric <283042246q@gmail.com>
2026-06-28 03:01:03 +0800

    1st commit

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 git submodule update --init --recursive --progress
 
-eval "$(~/miniconda3/bin/conda shell.bash hook)"
+eval "$(/home/eric/anaconda3/bin/conda shell.bash hook)"
 
 CONDA_ENV_NAME="mpd-splines-public"
 
@@ -26,7 +26,7 @@ conda update -n base conda
 
 conda env create -f environment.yml
 
-conda env config vars set CUDA_HOME=""
+conda env config vars set -n "${CONDA_ENV_NAME}" CUDA_HOME=""
 conda activate ${CONDA_ENV_NAME}
 
 echo "-------> Installing dependencies"
@@ -39,12 +39,12 @@ cd "${THIS_DIR}"/mpd/motion_planning_baselines && pip install -e .
 echo "-------> Installing pybullet_ompl"
 conda activate ${CONDA_ENV_NAME}
 cd "${DEPS_DIR}"/pybullet_ompl || exit 1
-git clone git@github.com:ompl/ompl.git
+git clone https://github.com/ompl/ompl.git
 cd ompl || exit 1
 git checkout fca10b4bd4840856c7a9f50d1ee2688ba77e25aa
 mkdir -p build/Release
 cd build/Release || exit 1
-cmake -DCMAKE_DISABLE_FIND_PACKAGE_pypy=ON ../.. -DPYTHON_EXEC=${HOME}/miniconda3/envs/${CONDA_ENV_NAME}/bin/python
+cmake -DCMAKE_DISABLE_FIND_PACKAGE_pypy=ON ../.. -DPYTHON_EXEC=/home/eric/anaconda3/envs/${CONDA_ENV_NAME}/bin/python
 make -j 32 update_bindings  # This step takes a lot of time.
 for _ in {1..5}; do make -j 32; done  # run multiple times to avoid errors
 cd ${DEPS_DIR}/pybullet_ompl && pip install -e .
